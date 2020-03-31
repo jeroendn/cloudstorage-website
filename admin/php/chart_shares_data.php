7e077ed0faa717f75250e99afca97f9401c6df48
jeroendn <51778824+jeroendn@users.noreply.github.com>
2020-03-31 14:02:24 +0200

    Shares chart working correctly

--- admin/php/chart_shares_data.php
+++ admin/php/chart_shares_data.php
@@ -2,18 +2,27 @@
 session_start();
 include_once '../../php/dbconnection.php';
 
-$sql = "SELECT COUNT(*) as count FROM share WHERE share_date BETWEEN '2020-03-01' AND '2020-03-31';";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-$march = $stmt->fetchAll();
+// get the last X months
+$month = time();
+for ($i = 1; $i <= 12; $i++) {
+  $month = strtotime('last month', $month);
+  $months[] = date("r", $month);
+}
 
+// flip the array to display from old to new
+$months = array_reverse($months, false);
 
+// get the data per month and write it to array
+foreach ($months as $month) {
+  $sql = "SELECT COUNT(*) as count FROM share WHERE share_date BETWEEN " . date("'Y-m-01'", strtotime($month)) . " AND " . date("'Y-m-31'", strtotime($month)) . "";
+  $stmt = $conn->prepare($sql);
+  $stmt->execute();
+  $month_count = $stmt->fetchAll();
 
-$year[] = array
- (
-    'Januari' => 5,
-    'Februari' => 3,
-    'March' => 9
- );
+  $count = $month_count[0]['count'];
+  settype($count, "integer");
+  // write the data to the array
+  $year[][date("M", strtotime($month))] = $count;
+}
 
 echo json_encode($year);
